648a164b6a2d9c4510a4d4de5723cdb85964e9be
jfagnwang <qbs1864@gmail.com>
2021-04-01 19:34:12 +0000

    install scripts checks if program is installed or not

--- auto_project_checker/selenium_install.sh
+++ auto_project_checker/selenium_install.sh
@@ -1,25 +1,35 @@
-echo "-------------------"
-echo "Installing Selenium"
-echo "-------------------"
 sudo apt-get update
-sudo apt-get -y install python3-pip
-sudo pip3 install selenium
 
-echo "------------------------------"
-echo "Installing Java, Chrome-stable"
-echo "------------------------------"
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt-get update 
-sudo apt-get install google-chrome-stable
-sudo apt-get -y install openjdk-7-jre google-chrome-stable xvfb unzip firefox
-echo "-------------------"
-echo "Installing Selenium"
-echo "-------------------"
-SELENIUM_VERSION=$(curl "https://selenium-release.storage.googleapis.com/" | perl -n -e'/.*<Key>([^>]+selenium-server-standalone[^<]+)/ && print $1')
-wget "https://selenium-release.storage.googleapis.com/${SELENIUM_VERSION}" -O selenium-server-standalone.jar
-chown vagrant:vagrant selenium-server-standalone.jar
-sudo mv selenium-server-standalone.jar /usr/local/bin
+if ! command -v pip &> /dev/null
+then
+    echo "==============="
+    echo "Installing pip3"
+    echo "==============="
+    sudo apt-get -y install python3-pip
+fi
+
+if ! command -v google-chrome-stable &> /dev/null
+then
+    echo "=============================="
+    echo "Installing Java, Chrome-stable"
+    echo "=============================="
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+    sudo apt-get install google-chrome-stable
+    sudo apt-get -y install openjdk-7-jre google-chrome-stable xvfb unzip firefox
+fi
+
+if ! command -v selenium &> /dev/null
+then
+    echo "==================="
+    echo "Installing Selenium"
+    echo "==================="
+    sudo pip3 install selenium
+    SELENIUM_VERSION=$(curl "https://selenium-release.storage.googleapis.com/" | perl -n -e'/.*<Key>([^>]+selenium-server-standalone[^<]+)/ && print $1')
+    wget "https://selenium-release.storage.googleapis.com/${SELENIUM_VERSION}" -O selenium-server-standalone.jar
+    chown vagrant:vagrant selenium-server-standalone.jar
+    sudo mv selenium-server-standalone.jar /usr/local/bin
+fi
 echo "------------------------"
 echo "Installing Chrome Driver"
 echo "------------------------"
@@ -28,7 +38,9 @@ wget "http://chromedriver.storage.googleapis.com/${CHROMEDRIVER_VERSION}/chromed
 unzip chromedriver_linux64.zip
 sudo rm chromedriver_linux64.zip
 chown vagrant:vagrant chromedriver
-sudo mv chromedriver /usr/local/bin
-export DISPLAY=:10
 
+export DISPLAY=:10
 sudo pip3 install --upgrade --ignore-installed urllib3
+echo "============================================="
+echo "You're good to go. Just run the main.py file."
+echo "============================================="
